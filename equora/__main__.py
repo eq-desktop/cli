f64688b9d4d2309b9582acd40ed0d4f740c5457a
Jonas Schmidt <schmidtjonas662@gmail.com>
2025-09-22 15:15:06 +0200

    feat: add post install step guide

--- equora/__main__.py
+++ equora/__main__.py
@@ -110,6 +110,9 @@ def install_equora():
         # mv ~/eqSh/eqsh ~/.config/quickshell/
         shutil.move(os.path.expanduser("~/eqSh/eqsh"), os.path.expanduser("~/.config/quickshell/"))
         ok("Equora installed")
+        print("Post-installation steps:")
+        print("- Install Quickshell https://quickshell.org")
+        print("- Run `eqsh run` to start Equora")
     except Exception as e:
         err(f"Failed to install Equora: {e}")
 
